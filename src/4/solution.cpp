5c3cd6db218e51551807b1a0960114f7095cc0e9
ZeroColapseD <glebmolyavko@gmail.com>
2026-08-14 17:55:05 +1000

    v2-fix_2

--- src/4/solution.cpp
+++ src/4/solution.cpp
@@ -1,20 +1,16 @@
 #include <string>
 #include <cctype>
 
-bool check_password(std::string str, int min_length){
-    bool symbol{false}; bool upper{false}; bool lower{false}; bool digit{false};
-    if(str.length() >= min_length){
-    for (char c : str) {
-        if(isdigit(c) == true){digit = true;}
-        if(c == '!' || c == '#' || c == '$'|| c == '%' || c == '^' || c == '*' ){symbol = true;}
-        if(islower(c)){lower = true;}
-        if(isupper(c)){upper = true;} 
-    }
-}
-    if(symbol && upper && lower && digit){
-        return true;
-    }else{
-        return false;
-    }
-
+bool check_password(std::string str, int min_length) {
+    bool symbol{false}, upper{false}, lower{false}, digit{false};
+    if (str.length() >= static_cast<size_t>(min_length)) {
+        for (char c : str) {
+            unsigned char uc = static_cast<unsigned char>(c);
+            if (std::isdigit(uc)) digit = true;
+            if (c == '!' || c == '#' || c == '$' || c == '%' || c == '^' || c == '*')
+                symbol = true;
+            if (std::islower(uc)) lower = true;
+            if (std::isupper(uc)) upper = true;
+            if (symbol && upper && lower && digit) break;}}
+    return symbol && upper && lower && digit;
 }
