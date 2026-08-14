ae1d25cea14983a10507958e315d34af3ab76d75
ZeroColapseD <glebmolyavko@gmail.com>
2026-08-14 17:45:02 +1000

    v2-fix_1

--- src/4/solution.cpp
+++ src/4/solution.cpp
@@ -2,16 +2,16 @@
 #include <cctype>
 
 bool check_password(std::string str, int min_length){
-    int symbol{0}; int Upper{0}; int lower{0}; int digit{0};
+    bool symbol{false}; bool upper{false}; bool lower{false}; bool digit{false};
     if(str.length() >= min_length){
     for (char c : str) {
-        if(isdigit(c) == true){digit+=1;}
-        if(c == '!' || c == '#' || c == '$'|| c == '%' || c == '^' || c == '*' ){symbol+=1;}
-        if(islower(c)){lower+=1;}
-        if(isupper(c)){Upper+=1;}
+        if(isdigit(c) == true){digit = true;}
+        if(c == '!' || c == '#' || c == '$'|| c == '%' || c == '^' || c == '*' ){symbol = true;}
+        if(islower(c)){lower = true;}
+        if(isupper(c)){upper = true;} 
     }
 }
-    if(symbol > 0 && Upper > 0 && lower > 0 && digit > 0){
+    if(symbol && upper && lower && digit){
         return true;
     }else{
         return false;
